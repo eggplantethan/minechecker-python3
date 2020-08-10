ec8f32a47dd7671f404cac151694676bb99f4c89
Owen <owen.herbert@hotmail.com>
2020-08-11 07:34:40 +1000

    Updated script to follow PEP8 style guide.

--- index.py
+++ index.py
@@ -7,31 +7,31 @@ import requests, json, time
 
 try:
     # Open unclaimed words dump file for appending:
-    unclaimedFile = open("unclaimed.txt", "a")
+    unclaimed_file = open("unclaimed.txt", "a")
 
     # Open words dump file for reading:
-    wordFile = open("words.txt", "r")
-    wordArray = wordFile.readlines()
+    word_file = open("words.txt", "r")
+    word_array = word_file.readlines()
 
     # Print some useful info:
-    print("[INFO] {0} words were read from the provided file.".format(len(wordArray)))
+    print("[INFO] {0} words were read from the provided file.".format(len(word_array)))
     print("[INFO] Starting checks. You will be notified when an account is added to the unclaimed file.")
 
     # Loop over each line in word array:
-    for x in range(len(wordArray)):
+    for x in range(len(word_array)):
 
-        word = wordArray[x].strip()
+        word = word_array[x].strip()
         response = requests.get("https://api.mojang.com/users/profiles/minecraft/{0}".format(word))
         if response.status_code == 204:
             print("[INFO] {0} is not taken.".format(word))
-            unclaimedFile.write(word + ",")
+            unclaimed_file.write(word + ",")
 
         # Sleep for one second (Only lower if your list is short):
         time.sleep(1)
 
     # Finish up:
     print("[INFO] All names have been checked.")
-    unclaimedFile.close()
+    unclaimed_file.close()
 
 except Exception as e:
     print('[INFO] An unexpected error occured. Words file might be corrupt.')
